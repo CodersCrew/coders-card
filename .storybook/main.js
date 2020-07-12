e71d8598351a33058c93f574404d0f7c6a888213
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2020-07-12 23:46:36 +0200

    Add storybook and create one example story (#28)

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.(ts|tsx)'],
+  addons: [
+    {
+      name: '@storybook/preset-typescript',
+      options: {
+        tsLoaderOptions: {
+          configFile: path.resolve(__dirname, '../tsconfig.json'),
+        },
+        include: [path.resolve(__dirname, '../src')],
+        transpileManager: true,
+      },
+    },
+    '@storybook/addon-actions',
+    '@storybook/addon-a11y/register',
+  ],
+};
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button as MuiButton, ButtonProps as MuiButtonProps } from '@material-ui/core';
+
+export type ButtonProps = MuiButtonProps;
+
+export const Button = (props: ButtonProps) => {
+  return <MuiButton {...props} />;
+}
