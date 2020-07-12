f0243322afa20d632ea20354a03e6b68b0903f37
Dominik <dominik.slawkowski@gmail.com>
2020-07-12 19:37:54 +0200

    Create basic Gatsby Skeleton with TypeScript (#29)
    * fixing typescript
    
    * convert components and pages to tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,14 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React, { ReactNode } from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import Header from "./header";
+import "./layout.css";
 
-import Header from "./header"
-import "./layout.css"
+type LayoutProps = {
+  children: ReactNode;
+};
 
-const Layout = ({ children }) => {
+const Layout = ({ children }: LayoutProps) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -21,7 +17,7 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
@@ -41,11 +37,7 @@ const Layout = ({ children }) => {
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default Layout;
