5d4b29aa20b233cf0ead4f482caff542b3b6384e
unknown <kaacper.janusz@gmail.com>
2020-07-20 22:51:06 +0200

    first version of Button Component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Smile } from 'react-feather';
-import { Button, ButtonProps } from ".";
+import { Button, ButtonProps } from '.';
 
 export default {
   title: 'Button',
@@ -13,12 +13,33 @@ const basePropsData: Partial<ButtonProps> = {
   color: 'primary',
   variant: 'contained',
   onClick: action('onClick'),
-}
+};
+const smallBasePropsData: Partial<ButtonProps> = {
+  color: 'primary',
+  variant: 'contained',
+  onClick: action('onClick'),
+  size: 'small',
+};
 
 export const TextOnly = () => {
-  return <Button {...basePropsData}>Text only</Button>
-}
+  return <Button {...basePropsData}>Text only</Button>;
+};
 
 export const WithIcon = () => {
-  return <Button {...basePropsData} startIcon={<Smile size={16} />}>With icon</Button>
-}
+  return (
+    <Button {...basePropsData} startIcon={<Smile size={16} />}>
+      With icon
+    </Button>
+  );
+};
+export const TextOnlySmall = () => {
+  return <Button {...smallBasePropsData}>Text only</Button>;
+};
+
+export const WithIconSmall = () => {
+  return (
+    <Button {...smallBasePropsData} startIcon={<Smile size={16} />}>
+      With icon
+    </Button>
+  );
+};
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import { Button as MuiButton, ButtonProps as MuiButtonProps } from '@material-ui/core';
+import { Button as MuiButton, ButtonProps as MuiButtonProps, makeStyles } from '@material-ui/core';
 
 export type ButtonProps = MuiButtonProps;
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    '&.MuiButton-containedPrimary': {
+      backgroundColor: theme.palette.primary.main,
+      padding: theme.spacing(1),
+    },
+    '&.MuiButton-contained': {
+      width: '158px',
+      height: '40x',
+    },
+    '&.MuiButton-containedSizeSmall': {
+      width: '150px',
+      height: '32px',
+    },
+  },
+}));
+
 export const Button = (props: ButtonProps) => {
-  return <MuiButton {...props} />;
-}
+  const classes = useStyles();
+
+  return <MuiButton className={classes.button} {...props} />;
+};
