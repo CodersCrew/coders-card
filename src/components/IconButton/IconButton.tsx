a0b479884c4aa7f0bb25df2e61ae1e1d8e8786b1
maciekkwiet <33134609+maciekkwiet@users.noreply.github.com>
2020-07-21 17:23:55 +0200

    Create IconButton component (#38)
    * first version
    
    * second version
    
    * next version
    
    * NewButtons
    
    * fix

---
+++ src/components/IconButton/IconButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { IconButton as MuiIconButton, IconButtonProps as MuiIconButtonProps, makeStyles } from '@material-ui/core';
+
+export type IconButtonProps = MuiIconButtonProps;
+
+const useStyles = makeStyles((theme) => ({
+  muiIconButton: {
+    color: theme.palette.background.paper,
+    border: 'solid 2px',
+    borderRadius: '8px',
+  },
+}));
+
+export const IconButton = (props: IconButtonProps) => {
+  const classes = useStyles();
+  return <MuiIconButton className={classes.muiIconButton} {...props} />;
+};
