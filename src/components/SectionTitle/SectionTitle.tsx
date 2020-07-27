749a8f9d8c092b5e3e556ca317d1ec83055bbd72
Celina Wierzbicka <celinawierzbicka@gmail.com>
2020-07-27 19:20:20 +0200

    minor code reformatting

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -4,11 +4,11 @@ import { Box, BoxProps, Typography, makeStyles } from '@material-ui/core';
 export type SectionTitleProps = BoxProps;
 
 const useStyles = makeStyles((theme) => ({
-  SectionTitle: {
+  sectionTitle: {
     color: theme.palette.text.primary,
   },
   divider: {
-    height: '2px',
+    height: 2,
     marginTop: theme.spacing(1.5),
     background: `linear-gradient(left, ${theme.palette.primary.main} 0%, ${theme.palette.primary.main} 64px, ${theme.palette.divider} 64px, ${theme.palette.divider} 100%)`,
   },
@@ -18,7 +18,7 @@ export const SectionTitle = (props: SectionTitleProps) => {
   const classes = useStyles();
   return (
     <Typography variant="h3">
-      <Box className={classes.SectionTitle} {...props} >
+      <Box className={classes.sectionTitle} {...props} >
         {props.children}
         <div className={classes.divider} />
       </Box>
