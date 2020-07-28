fb9c26e845a03c446e92b166d82518e0f7072a2c
zofianatanek <zofia.natanek@gmail.com>
2020-07-28 20:59:49 +0200

    Improve styles

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Chip as MuiChip, ChipProps as MuiChipProps, makeStyles, withTheme } from '@material-ui/core';
+import { Chip as MuiChip, ChipProps as MuiChipProps, makeStyles } from '@material-ui/core';
 export type TagProps = MuiChipProps;
 
 const useStyles = makeStyles((theme) => ({
   MuiChip: {
+    ...theme.typography.subtitle2,
     backgroundColor: theme.palette.background.tag,
     borderRadius: '4px',
-    color: 'white'
+    height: '24px',
+    color: theme.palette.text.white,
+    ...theme.typography.subtitle2
   },
 }));
 export const Tag = (props: TagProps) => {
   const classes = useStyles();
+
   return <MuiChip className={classes.MuiChip} {...props} />;
 }
