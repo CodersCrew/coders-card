5c60fe6c89f6d2ae3980bc7c0834a56a8db11ddb
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-07-28 11:53:53 +0200

    first style

--- src/components/Testimonial/TestimonialDefault.tsx
+++ src/components/Testimonial/TestimonialDefault.tsx
@@ -8,35 +8,19 @@ const useStyles = makeStyles((theme) => ({
   card: {
     width: '100%',
     height: '100%',
-    borderRadius: 8,
-    boxShadow: '0 8px 20px 0 rgba(68, 86, 108, 0.1)',
   },
   media: {
-    height: '100%',
-    width: '100%',
     display: 'flex',
     alignItems: 'flex-end',
     justifyContent: 'flex-end',
-  },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: '100%',
-    width: '100%',
-    transition: '.5s ease',
-    backgroundColor: theme.palette.primary.dark,
-  },
-  title: {
-    color: theme.palette.text.white,
-    marginBottom: theme.spacing(2),
-    marginLeft: theme.spacing(1),
+    width: 64,
+    height: 64,
+    borderRadius: 8,
   },
   description: {
-    color: theme.palette.text.white,
-    marginLeft: theme.spacing(1),
+    color: theme.palette.text.secondary,
+    margin: theme.spacing(3),
+    fontFamily: 'Inter-Italic',
   },
   label: {
     display: 'flex',
@@ -47,27 +31,27 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.white,
     borderRadius: 8,
   },
-  icon: {
-    color: theme.palette.background.paper,
+  labelBold: {
+    display: 'flex',
   },
 }));
 
 type Props = TestimonialProps;
 
-const TestimonialDefault: FC<Props> = ({ label, description, image, ...props }) => {
+const TestimonialDefault: FC<Props> = ({ label, labelBold, description, image, ...props }) => {
   const classes = useStyles();
   return (
     <Card className={classes.card} {...props}>
+      <Typography className={classes.description} variant="body2">
+        {description}
+      </Typography>
       <CardMedia className={classes.media} image={image}></CardMedia>
-      <CardContent className={classes.overlay}>
-        <Typography className={classes.title} variant="h4"></Typography>
-        <Typography className={classes.description} variant="body2">
-          {description}
-        </Typography>
-        <Typography className={classes.label} variant="subtitle2">
-          <span>{label}</span>
-        </Typography>
-      </CardContent>
+      <Typography className={classes.labelBold} variant="h6">
+        {labelBold}
+      </Typography>
+      <Typography className={classes.label} variant="subtitle2">
+        {label}
+      </Typography>
     </Card>
   );
 };
--- src/components/Testimonial/TestimonialMobile.tsx
+++ src/components/Testimonial/TestimonialMobile.tsx
@@ -8,60 +8,60 @@ const useStyles = makeStyles((theme) => ({
   card: {
     width: '100%',
     height: '100%',
-    borderRadius: 8,
-    boxShadow: '0 8px 20px 0 rgba(68, 86, 108, 0.1)',
   },
   media: {
-    width: '100%',
-    minHeight: 152,
     display: 'flex',
-    alignItems: 'flex-end',
-    justifyContent: 'flex-end',
-  },
-  content: {
-    height: '100%',
-  },
-  title: {
-    color: theme.palette.text.primary,
-    marginBottom: theme.spacing(1),
+    justifyContent: 'center',
+    alignItems: 'center',
+    //justifyContent: 'space-between',
+    //width: 64,
+    width: '100%',
+    height: 64,
+    borderRadius: 8,
   },
   description: {
     color: theme.palette.text.secondary,
+    margin: theme.spacing(3),
+    fontFamily: 'Inter-Italic',
   },
   label: {
     display: 'flex',
-    alignItems: 'center',
-    margin: theme.spacing(1),
-    padding: theme.spacing(0.5, 1.5, 0.5, 1),
-    color: theme.palette.text.secondary,
-    backgroundColor: theme.palette.background.paper,
-    borderRadius: 8,
+    color: theme.palette.text.primary,
+    fontFamily: 'Inter-Regular',
+    // alignItems: 'center',
+    // justifyContent: 'space-between',
+    // margin: theme.spacing(1),
+    // marginTop: theme.spacing(7),
+    // borderRadius: 8,
   },
-  icon: {
-    marginRight: theme.spacing(1),
-    color: theme.palette.text.secondary,
-    height: 16,
+  labelBold: {
+    display: 'flex',
+    color: theme.palette.text.primary,
+    fontFamily: 'Inter-Bold',
+    // alignItems: 'center',
+    // justifyContent: 'space-between',
+    // margin: theme.spacing(1),
+    // marginTop: theme.spacing(7),
+    // borderRadius: 8,
   },
 }));
 
 type Props = TestimonialProps;
 
-const TestimonialMobile: FC<Props> = ({ label, description, image, ...props }) => {
+const TestimonialMobile: FC<Props> = ({ label, labelBold, description, image, ...props }) => {
   const classes = useStyles();
-
   return (
-    <Card {...props} className={classes.card}>
-      <CardMedia className={classes.media} image={image}>
-        <Typography className={classes.label} variant="subtitle2">
-          <span>{label}</span>
-        </Typography>
-      </CardMedia>
-      <CardContent className={classes.content}>
-        <Typography className={classes.title} variant="h4"></Typography>
-        <Typography className={classes.description} variant="body2">
-          {description}
-        </Typography>
-      </CardContent>
+    <Card className={classes.card} {...props}>
+      <CardMedia className={classes.media} image={image}></CardMedia>
+      <Typography className={classes.description} variant="body2">
+        {description}
+      </Typography>
+      <Typography className={classes.labelBold} variant="h6">
+        {labelBold}
+      </Typography>
+      <Typography className={classes.label} variant="body2">
+        {label}
+      </Typography>
     </Card>
   );
 };
