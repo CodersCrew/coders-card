c9c40ea9f421add92d03d3c27b0be458d450db67
mecies <maciekhnat@gmail.com>
2020-08-01 20:58:15 +0200

    [PortfolioPage]: Add tablet version of the page

---
+++ src/pages/portfolio-page/data.ts
@@ -0,0 +1,31 @@
+import { CardProps } from '../../components/DetailsCard/cardProps';
+import avatar from '../../images/avatar.jpg';
+import project from '../../images/download.jpeg';
+
+export const projectData = {
+  image: project,
+  title: 'My Awesome Project',
+  description:
+    'In vel tellus ac quam elementum vulputate. Proin quis eros in elit luctus tempor. Aenean in hendrerit metus. Donec congue enim a dui efficitur, a pellentesque.',
+  label: 'Mobile app',
+  onClick: () => {
+    window.alert('project clicked');
+  },
+};
+
+export const userData: CardProps = {
+  fullName: 'Jonathan Harwood',
+  image: avatar,
+  position: 'Full stack developer',
+  socialMedia: [
+    { name: 'facebook', link: 'https://www.facebook.com/' },
+    { name: 'github', link: 'https://github.com/' },
+    { name: 'twitter', link: 'https://twitter.com/' },
+    { name: 'instagram', link: 'https://www.instagram.com/' },
+  ],
+  phone: '+48 601 345 132',
+  email: 'jonathan.harwood@gmail.com',
+  address: 'Wroclaw, Poland',
+  isFreelancer: true,
+  resumeLink: 'https://www.docdroid.net/WyjIuyO/fake-resume-pdf',
+};
--- src/pages/portfolio-page/index.tsx
+++ src/pages/portfolio-page/index.tsx
@@ -1,111 +1,119 @@
 import React from 'react';
-import { Container, Box, makeStyles, Card } from '@material-ui/core';
+import { Container, Box, makeStyles, Card, useTheme, useMediaQuery } from '@material-ui/core';
 
 import { DetailsCard } from '../../components/DetailsCard';
 import { SectionTitle } from '../../components/SectionTitle';
-import { CardProps } from '../../components/DetailsCard/cardProps';
 import { PortfolioCard } from '../../components/PortfolioCard';
 
-import avatar from '../../images/avatar.jpg';
-import project from '../../images/download.jpeg';
+import { projectData, userData } from './data';
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    display: 'flex',
-    minHeight: '100vh',
-    paddingTop: theme.spacing(8),
+    padding: 0,
+    [theme.breakpoints.up('lg')]: {
+      display: 'flex',
+      minHeight: '100vh',
+      padding: theme.spacing(8, 2, 0, 2),
+    },
   },
   aside: {
-    height: 668,
-    width: 280,
-    marginRight: theme.spacing(4),
+    display: 'none',
+    [theme.breakpoints.up('lg')]: {
+      display: 'block',
+      position: 'sticky',
+      top: theme.spacing(8),
+      height: 668,
+      width: 280,
+      marginRight: theme.spacing(4),
+    },
   },
   main: {
-    width: '100%',
+    [theme.breakpoints.up('lg')]: {
+      width: '100%',
+    },
   },
   mainContent: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  navbar: {
-    // temporary styles
-    height: 56,
-    width: '100%',
-    boxShadow: '0 40px 50px 0 rgba(103, 118, 128, 0.1)',
-    borderRadius: 8,
-    marginBottom: theme.spacing(7),
-  },
-  projectsContainer: {
-    // temporary styles
-
-    width: '100%',
     boxShadow: '0 40px 50px 0 rgba(103, 118, 128, 0.1)',
     borderRadius: 16,
-    padding: theme.spacing(4),
-    marginBottom: theme.spacing(7),
+    margin: theme.spacing(0, 3, 7, 3),
+
+    [theme.breakpoints.up('lg')]: {
+      display: 'flex',
+      flexDirection: 'column',
+      margin: theme.spacing(0, 0, 7, 0),
+    },
+  },
+  navbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    boxShadow: '0 40px 50px 0 rgba(103, 118, 128, 0.1)',
+    borderRadius: 8,
+    height: 120,
+    width: '100%',
+    marginBottom: theme.spacing(4),
+
+    [theme.breakpoints.up('lg')]: {
+      marginBottom: theme.spacing(7),
+      height: 56,
+    },
+  },
+  projectsContainer: {
+    boxShadow: '0 40px 50px 0 rgba(103, 118, 128, 0.1)',
+    borderRadius: 16,
+    padding: theme.spacing(3),
+
+    [theme.breakpoints.up('lg')]: {
+      width: '100%',
+      padding: theme.spacing(4),
+    },
   },
   projects: {
     display: 'grid',
-    gridTemplateColumns: '400px 400px',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gridGap: theme.spacing(5),
+    gridTemplateColumns: '1fr',
+    gridRowGap: theme.spacing(3),
+
+    [theme.breakpoints.up('lg')]: {
+      gridTemplateColumns: '400px 400px',
+      alignItems: 'center',
+      justifyContent: 'center',
+      gridGap: theme.spacing(5),
+    },
   },
   project: {
-    width: 400,
-    height: 224,
+    [theme.breakpoints.up('lg')]: {
+      width: 400,
+      height: 224,
+    },
   },
   title: {
-    padding: theme.spacing(0, 2, 4, 2),
+    marginBottom: theme.spacing(3),
+
+    [theme.breakpoints.up('lg')]: {
+      margin: theme.spacing(0, 2, 4, 2),
+    },
   },
 }));
 
-const portfolioCardProps = {
-  image: project,
-  title: 'My Awesome Project',
-  description:
-    'In vel tellus ac quam elementum vulputate. Proin quis eros in elit luctus tempor. Aenean in hendrerit metus. Donec congue enim a dui efficitur, a pellentesque.',
-  label: 'Mobile app',
-  onClick: () => {
-    window.alert('siemano');
-  },
-};
-
-const detailsCardProps: CardProps = {
-  fullName: 'Jonathan Harwood',
-  image: avatar,
-  position: 'Full stack developer',
-  socialMedia: [
-    { name: 'facebook', link: 'https://www.facebook.com/' },
-    { name: 'github', link: 'https://github.com/' },
-    { name: 'twitter', link: 'https://twitter.com/' },
-    { name: 'instagram', link: 'https://www.instagram.com/' },
-  ],
-  phone: '+48 601 345 132',
-  email: 'jonathan.harwood@gmail.com',
-  address: 'Wroclaw, Poland',
-  isFreelancer: true,
-  resumeLink: 'https://www.docdroid.net/WyjIuyO/fake-resume-pdf',
-};
-
 const IndexPage = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
+  const isTablet = useMediaQuery(theme.breakpoints.down('md'));
+  const isDesktop = !isMobile && !isTablet;
+
   return (
     <Container className={classes.container} maxWidth="lg">
-      <Box className={classes.aside}>
-        <DetailsCard type="desktop" {...detailsCardProps} />
-      </Box>
+      <Box className={classes.aside}>{isDesktop && <DetailsCard type="desktop" {...userData} />}</Box>
       <Box className={classes.main}>
-        <Card className={classes.navbar} />
+        <Card className={classes.navbar}>navbar</Card>
         <Card className={classes.mainContent}>
           <Box className={classes.projectsContainer}>
-            <Box className={classes.title}>
-              <SectionTitle>My works</SectionTitle>
-            </Box>
+            <SectionTitle className={classes.title}>My works</SectionTitle>
             <Box className={classes.projects}>
               {[1, 2, 3, 4, 5, 6].map((_) => (
                 <Box key={_} className={classes.project}>
-                  <PortfolioCard type="desktop" {...portfolioCardProps} />
+                  {<PortfolioCard type={isMobile ? 'mobile' : isTablet ? 'tablet' : 'desktop'} {...projectData} />}
                 </Box>
               ))}
             </Box>
