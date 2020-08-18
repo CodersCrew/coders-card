4497d24dd8d22a4d7f1627c47a06938ed9708338
Dominik Slawkowski <dominik.slawkowski@gmail.com>
2020-08-18 16:48:12 +0200

    add first version of about page

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -10,6 +10,7 @@ const useStyles = makeStyles((theme) => ({
   divider: {
     height: 2,
     marginTop: theme.spacing(1.5),
+    marginLeft: 0,
     background: `linear-gradient(left, ${theme.palette.primary.main} 0%, ${theme.palette.primary.main} 64px, ${theme.palette.divider} 64px, ${theme.palette.divider} 100%)`,
   },
 }));
@@ -18,10 +19,10 @@ export const SectionTitle = (props: SectionTitleProps) => {
   const classes = useStyles();
   return (
     <Typography variant="h3">
-      <Box className={classes.sectionTitle} {...props} >
+      <Box className={classes.sectionTitle} {...props}>
         {props.children}
         <div className={classes.divider} />
       </Box>
     </Typography>
-  )
-}
+  );
+};
