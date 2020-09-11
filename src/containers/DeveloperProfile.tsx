675280921094620c6299bf898def322a5fc9685e
mecies <maciekhnat@gmail.com>
2020-09-11 21:51:20 +0200

    Change developer profile type directory

--- src/containers/DeveloperProfile.tsx
+++ src/containers/DeveloperProfile.tsx
@@ -1,6 +1,25 @@
 import React, { createContext, PropsWithChildren, useContext } from 'react';
 
-import { DeveloperProfileGQL } from '../components/DetailsCard/types';
+export type DeveloperProfileGQL = {
+  last_name: string;
+  is_freelancer: boolean;
+  first_name: string;
+  email: string;
+  phone: string;
+  country: string;
+  city: string;
+  avatar: {
+    publicURL: string;
+  };
+  social_media: {
+    facebook: string;
+    github: string;
+    instagram: string;
+    twitter: string;
+  };
+  position: string;
+  cv: string;
+};
 
 type DeveloperProfile = DeveloperProfileGQL | null;
 
