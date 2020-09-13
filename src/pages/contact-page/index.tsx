f8167af23ced05f469df0c93a9f410a85e35ca42
unknown <kaacper.janusz@gmail.com>
2020-09-13 16:52:58 +0200

    comments and other unnecessary elements deleted

--- src/pages/contact-page/index.tsx
+++ src/pages/contact-page/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useFormik, ErrorMessage, Formik, Form, Field } from 'formik';
-// import { TextField } from 'formik-material-ui';
-import { fieldToTextField, TextFieldProps } from 'formik-material-ui';
 
+import {  Formik, Form, Field } from 'formik';
+import { fieldToTextField, TextFieldProps } from 'formik-material-ui';
+import { Container, Box, makeStyles, Card, useTheme, useMediaQuery } from '@material-ui/core';
 import * as Yup from 'yup';
 import { Helmet } from 'react-helmet';
-import { Container, Box, makeStyles, Card, useTheme, useMediaQuery } from '@material-ui/core';
 import { Send } from 'react-feather';
+
 import { DetailsCard } from '../../components/DetailsCard';
 import { SectionTitle } from '../../components/SectionTitle';
 import { TextField } from '../../components/TextField';
@@ -15,10 +15,6 @@ import { userData, textFieldData } from './data';
 
 const contactPageItemShadow = '0 40px 50px 0 rgba(103, 118, 128, 0.1)';
 
-// interface Values {
-//   email: string;
-//   password: string;
-// }
 
 function TextFieldWrap(props: TextFieldProps) {
   const {
@@ -42,6 +38,20 @@ interface FormValues {
   messageContent: string;
 }
 
+const initialValues: FormValues = {
+  fullName: '',
+  email: '',
+  title: '',
+  messageContent: '',
+};
+
+const validationSchema = Yup.object().shape({
+  fullName: Yup.string().required('Full Name is required field'),
+  email: Yup.string().required('E-mail adress is required field').email(),
+  title: Yup.string().required('Title is required field'),
+  messageContent: Yup.string().required('Message content is required field'),
+});
+
 const useStyles = makeStyles((theme) => ({
   container: {
     padding: 0,
@@ -56,15 +66,14 @@ const useStyles = makeStyles((theme) => ({
       width: '100%',
     },
   },
+
   detailsCard: {
     marginBottom: theme.spacing(2.5),
-
     [theme.breakpoints.up('lg')]: {
       height: 668,
       width: 280,
       display: 'block',
       position: 'sticky',
-
       marginRight: theme.spacing(4),
     },
     [theme.breakpoints.up('sm')]: {
@@ -72,50 +81,42 @@ const useStyles = makeStyles((theme) => ({
       position: 'sticky',
     },
   },
+
   mainContent: {
     backgroundColor: theme.palette.background.paper,
     borderRadius: 16,
-
+    padding: theme.spacing(3),
     [theme.breakpoints.up('lg')]: {
       display: 'flex',
       flexDirection: 'column',
       margin: theme.spacing(0, 0, 7, 0),
       boxShadow: contactPageItemShadow,
+      width: '100%',
+      padding: theme.spacing(4),
     },
   },
+
   navbar: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     boxShadow: contactPageItemShadow,
-
     height: 120,
     width: '100%',
     marginBottom: theme.spacing(2),
-
     [theme.breakpoints.up('sm')]: {
       marginBottom: theme.spacing(4),
     },
-
     [theme.breakpoints.up('lg')]: {
       marginBottom: theme.spacing(7),
       height: 56,
     },
   },
-  projectsContainer: {
-    borderRadius: 16,
-    padding: theme.spacing(3),
 
-    [theme.breakpoints.up('lg')]: {
-      width: '100%',
-      padding: theme.spacing(4),
-    },
-  },
   inputs: {
     display: 'grid',
     gridTemplateRows: '1fr',
     gridRowGap: theme.spacing(4),
-
     [theme.breakpoints.up('sm')]: {
       gridRowGap: theme.spacing(4),
     },
@@ -124,70 +125,50 @@ const useStyles = makeStyles((theme) => ({
       gridGap: theme.spacing(4),
     },
   },
+
   nameAndEmail: {
     display: 'grid',
     gridGap: theme.spacing(3),
     '&.MuiFormControl-fullWidth': {
       margin: theme.spacing(0, 2, 4, 2),
     },
-
     [theme.breakpoints.up('sm')]: {
       justifyContent: 'space-between',
       gridTemplateColumns: '1fr 1fr',
       gridGap: theme.spacing(4),
     },
-
     [theme.breakpoints.up('lg')]: {
       gridTemplateColumns: '1fr 1fr',
       gridGap: theme.spacing(5),
       justifyContent: 'space-between',
     },
   },
-
-  project: {
-    [theme.breakpoints.up('lg')]: {
-      width: 400,
-      height: 224,
-    },
-  },
+ 
   buttonWrapper: {
     display: 'flex',
     width: '100%',
     justifyContent: 'flex-end',
     paddingTop: theme.spacing(4),
   },
+
   button: {
     width: 158,
     height: 40,
     borderRadius: 8,
     boxShadow: '0 2px 4px 0 rgba(47, 84, 235, 0.15), 0 8px 16px 0 rgba(47, 84, 235, 0.15)',
   },
+
   title: {
     marginBottom: theme.spacing(4),
-
     [theme.breakpoints.up('sm')]: {
       marginBottom: theme.spacing(3),
     },
-
     [theme.breakpoints.up('lg')]: {
       margin: theme.spacing(0, 2, 4, 2),
     },
   },
 }));
 
-const validationSchema = Yup.object().shape({
-  fullName: Yup.string().required('Full Name is required field'),
-  email: Yup.string().required('E-mail adress is required field').email(),
-  title: Yup.string().required('Title is required field'),
-  messageContent: Yup.string().required('Message content is required field'),
-});
-
-const initialValues: FormValues = {
-  fullName: '',
-  email: '',
-  title: '',
-  messageContent: '',
-};
 
 const ContactPage = () => {
   const classes = useStyles();
@@ -223,9 +204,8 @@ const ContactPage = () => {
         )}
 
         <Box className={classes.mainContent}>
-          <Box className={classes.projectsContainer}>
+          <Box>
             <SectionTitle className={classes.title}>Contact</SectionTitle>
-
             <Formik
               initialValues={initialValues}
               validationSchema={validationSchema}
