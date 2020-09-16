0eae97cb03fce4ed880ccef664b0c9e2d03af031
KlaraGajaszek <klaragajaszek21@wp.pl>
2020-09-16 18:32:27 +0200

    add JSX element

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -201,33 +201,35 @@ const ContactPage = () => {
       )}
       <Box className={classes.main}>
         {(isMobile || isTablet) && (
+          <>
             <Navbar
-            className={classes.navbar}
-            type={componentType}
-            fullName={`${developerProfile.firstName} ${developerProfile.lastName}`}
-            position={developerProfile.position}
-            image={developerProfile.avatar.publicURL}
-            resumeLink={developerProfile.cv}
-          />
-          <Box className={classes.detailsCard}>
-            <DetailsCard
-              fullName={`${developerProfile.firstName} ${developerProfile.lastName}`}
-              address={`${developerProfile.city}, ${developerProfile.country}`}
-              image={developerProfile.avatar.publicURL}
-              position={developerProfile.position}
-              socialMedia={[
-                { name: 'facebook', link: developerProfile.socialMedia.facebook },
-                { name: 'github', link: developerProfile.socialMedia.github },
-                { name: 'twitter', link: developerProfile.socialMedia.twitter },
-                { name: 'instagram', link: developerProfile.socialMedia.instagram },
-              ]}
-              phone={developerProfile.phone}
-              email={developerProfile.email}
-              isFreelancer={developerProfile.isFreelancer}
-              resumeLink={developerProfile.cv}
+              className={classes.navbar}
               type={componentType}
+              fullName={`${developerProfile.firstName} ${developerProfile.lastName}`}
+              position={developerProfile.position}
+              image={developerProfile.avatar.publicURL}
+              resumeLink={developerProfile.cv}
             />
-          </Box>
+            <Box className={classes.detailsCard}>
+              <DetailsCard
+                fullName={`${developerProfile.firstName} ${developerProfile.lastName}`}
+                address={`${developerProfile.city}, ${developerProfile.country}`}
+                image={developerProfile.avatar.publicURL}
+                position={developerProfile.position}
+                socialMedia={[
+                  { name: 'facebook', link: developerProfile.socialMedia.facebook },
+                  { name: 'github', link: developerProfile.socialMedia.github },
+                  { name: 'twitter', link: developerProfile.socialMedia.twitter },
+                  { name: 'instagram', link: developerProfile.socialMedia.instagram },
+                ]}
+                phone={developerProfile.phone}
+                email={developerProfile.email}
+                isFreelancer={developerProfile.isFreelancer}
+                resumeLink={developerProfile.cv}
+                type={componentType}
+              />
+            </Box>
+          </>
         )}
         <Box className={classes.mainContent}>
           <Box>
