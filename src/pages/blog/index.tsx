d89de2ecf66a571c223327c913fdd55976703fbb
Maciej Hnat <maciej.hnat@infermedica.com>
2020-10-13 19:28:54 +0200

    [fix]: Prevent window reference during production build

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -123,9 +123,12 @@ const BlogPage: FC<{ data: BlogGQL }> = ({ data }) => {
   const developerProfile = useDeveloperProfile();
   const [selectedBlogpost, setSelectedBlogpost] = useState(-1);
 
-  if (blogData.blogPost?.length === 0) {
-    navigate('/');
+  if (typeof window !== `undefined`) {
+    if (blogData.blogPost?.length === 0) {
+      navigate('/');
+    }
   }
+
   // no blogpost will have index equal to -1 therefore no blogpost will be selected
   const handleCloseBlogpost = () => {
     setSelectedBlogpost(-1);
