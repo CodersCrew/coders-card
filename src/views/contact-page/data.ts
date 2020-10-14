7f2f2b3eed51b4d178ff58c5b3551124dfbe33a0
Maciej Hnat <maciej.hnat@infermedica.com>
2020-10-14 21:08:57 +0200

    [chore]: Remove unused constant

--- src/views/contact-page/data.ts
+++ src/views/contact-page/data.ts
@@ -1,16 +1,4 @@
 import { TextFieldProps } from '../../components/TextField/TextField';
-import project from '../../images/download.jpeg';
-
-export const projectData = {
-  image: project,
-  title: 'My Awesome Project',
-  description:
-    'In vel tellus ac quam elementum vulputate. Proin quis eros in elit luctus tempor. Aenean in hendrerit metus. Donec congue enim a dui efficitur, a pellentesque.',
-  label: 'Mobile app',
-  onClick: () => {
-    console.log('project clicked');
-  },
-};
 
 export const textFieldData: TextFieldProps = {
   variant: 'filled',
