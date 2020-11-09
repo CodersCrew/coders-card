531903a793bd63890fcb3b9b5b78474d4ed129a3
Celina <40033385+celinawierzbicka@users.noreply.github.com>
2020-11-09 10:56:09 +0100

    Use home page as fallback route (#128)
    * Redirect to homepage
    
    * Return empty div

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+const NotFoundPage = () => {
+  useEffect(() => {
+    navigate('/');
+  }, []);
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+  return <div />;
+};
 
 export default NotFoundPage;
