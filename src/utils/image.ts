2ab59132250df5a25c0d01d93ab746c9c86003eb
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-05-13 17:00:13 +0200

    Add comment to describe `transformImage` logic

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -4,5 +4,6 @@ type TransformImageOptions = {
   resize?: 'fit' | 'smartcrop';
 };
 
+// We increase the requested image size by 1.2 factor as Netlify provides compression that lowers its quality.
 export const transformImage = (imageSrc: string, { width, height, resize = 'fit' }: TransformImageOptions) =>
   `${imageSrc}?nf_resize=${resize}&w=${Math.round(width * 1.2)}&h=${Math.round(height * 1.2)}`;
