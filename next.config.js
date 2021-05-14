64dbcf515012b423850d78d263a63397a5462b39
KonradSzwarc <konrad.szwarc.dev@gmail.com>
2021-05-14 10:38:42 +0200

    Use Preact for production builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+const withPreact = (config, { dev, isServer }) => {
+  if (!dev && !isServer) {
+    Object.assign(config.resolve.alias, {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
+    });
+  }
+
+  return config;
+};
 
 const enhanceWebpackConfig = (config) => {
   config.module.rules.push({
@@ -11,8 +19,17 @@ const enhanceWebpackConfig = (config) => {
   return config;
 };
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 module.exports = withBundleAnalyzer({
   future: { webpack5: true },
   target: 'serverless',
-  webpack: enhanceWebpackConfig,
+  webpack: (config, options) => {
+    let enhancedConfig = withPreact(config, options);
+    enhancedConfig = enhanceWebpackConfig(config);
+
+    return enhancedConfig;
+  },
 });
